f1c235d7e2193c1cb3a2dba2b296b2b884e4944a
hyee0715 <hyee0715@naver.com>
2023-10-23 17:14:03 +0900

    initial commit

---
+++ src/main/java/com/palpal/dealightbe/DealightBeApplication.java
@@ -0,0 +1,13 @@
+package com.palpal.dealightbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealightBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealightBeApplication.class, args);
+	}
+
+}
