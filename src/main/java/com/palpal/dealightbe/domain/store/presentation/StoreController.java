d967d795e0220c4ed770bb027155bef355f7fcca
Hongseob Lee <sjun2918@naver.com>
2023-10-30 18:49:23 +0900

    feat: Store 마이페이지 조회 컨트롤러 구현

--- src/main/java/com/palpal/dealightbe/domain/store/presentation/StoreController.java
+++ src/main/java/com/palpal/dealightbe/domain/store/presentation/StoreController.java
@@ -2,6 +2,7 @@ package com.palpal.dealightbe.domain.store.presentation;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.palpal.dealightbe.domain.store.application.StoreService;
 import com.palpal.dealightbe.domain.store.application.dto.request.StoreCreateReq;
 import com.palpal.dealightbe.domain.store.application.dto.response.StoreCreateRes;
+import com.palpal.dealightbe.domain.store.application.dto.response.StoreInfoRes;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,5 +28,12 @@ public class StoreController {
 		StoreCreateRes storeCreateRes = storeService.register(memberId, req);
 
 		return ResponseEntity.ok(storeCreateRes);
+	}
+
+	@GetMapping("/{memberId}/{storeId}")
+	public ResponseEntity<StoreInfoRes> getInfo(@PathVariable Long memberId, @PathVariable Long storeId) {
+		StoreInfoRes infoRes = storeService.getInfo(memberId, storeId);
+
+		return ResponseEntity.ok(infoRes);
 	}
 }
