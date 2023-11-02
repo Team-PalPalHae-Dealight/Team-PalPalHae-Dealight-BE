8647fc7c90015e5b275e3ac645e18dbb3868bd71
ysng_ysng <yeasung67@gmail.com>
2023-11-02 11:19:05 +0900

    ci: 도커컴포즈가 제대로 작동안하는 문제해결

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,25 +3,25 @@ cd /home/ubuntu/app
 DOCKER_APP_NAME=dealight-api-springboot
 
 # 실행중인 blue가 있는지
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
+EXIST_BLUE=$(docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
 
 # green이 실행중이면 blue up
 if [ -z "$EXIST_BLUE" ]; then
   echo "Green 컨테이너가 실행 중이므로 Blue 컨테이너를 실행(UP)합니다..."
-  docker-compose -f docker-compose.blue.yml up -d --build
+  docker compose -f docker-compose.blue.yml up -d --build
 
   sleep 30
 
-  docker-compose -f docker-compose.green.yml down
+  docker compose -f docker-compose.green.yml down
   docker image prune -af # 사용하지 않는 이미지 삭제
 
 # blue가 실행중이면 green up
 else
   echo "Blue 컨테이너가 실행 중이므로 Green 컨테이너를 실행(UP)합니다..."
-  docker-compose -f docker-compose.green.yml up -d --build
+  docker compose -f docker-compose.green.yml up -d --build
 
   sleep 30
 
-  docker-compose -f docker-compose.blue.yml down
+  docker compose -f docker-compose.blue.yml down
   docker image prune -af
 fi
