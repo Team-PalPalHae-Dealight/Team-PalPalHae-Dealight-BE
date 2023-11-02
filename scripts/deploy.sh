341cd17ccc40cfb4c21c5b07d6aba9d496c506ac
ysng_ysng <yeasung67@gmail.com>
2023-11-02 13:41:23 +0900

    ci: CI/CD 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-chmod 755 -R /home/ubuntu/app
 cd /home/ubuntu/app
 DOCKER_APP_NAME=dealight
 
@@ -9,20 +8,20 @@ EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.ym
 # green이 실행중이면 blue up
 if [ -z "$EXIST_BLUE" ]; then
   echo "Green 컨테이너가 실행 중이므로 Blue 컨테이너를 실행(UP)합니다..."
-  docker-compose -f docker-compose.blue.yml up -d
+  docker compose -f docker-compose.blue.yml up -d
 
   sleep 30
 
-  docker-compose -f docker-compose.green.yml down
+  docker compose -f docker-compose.green.yml down
   docker image prune -af # 사용하지 않는 이미지 삭제
 
 # blue가 실행중이면 green up
 else
   echo "Blue 컨테이너가 실행 중이므로 Green 컨테이너를 실행(UP)합니다..."
-  docker-compose -f docker-compose.green.yml up -d
+  docker compose -f docker-compose.green.yml up -d
 
   sleep 30
 
-  docker-compose -f docker-compose.blue.yml down
+  docker compose -f docker-compose.blue.yml down
   docker image prune -af
 fi
