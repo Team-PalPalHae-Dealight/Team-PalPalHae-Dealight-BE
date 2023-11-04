d3d52c00e9a49799c2ee46e5fcdb16a0f0010fd6
SW-H <hsw08@naver.com>
2023-11-04 15:16:23 +0900

    test : 업체가 주문 목록을 조회할 수 있다

--- src/test/java/com/palpal/dealightbe/domain/order/application/OrderServiceTest.java
+++ src/test/java/com/palpal/dealightbe/domain/order/application/OrderServiceTest.java
@@ -2,16 +2,20 @@ package com.palpal.dealightbe.domain.order.application;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.Set;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -19,17 +23,23 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.palpal.dealightbe.domain.member.domain.Member;
 import com.palpal.dealightbe.domain.member.domain.MemberRepository;
 import com.palpal.dealightbe.domain.order.application.dto.request.OrderStatusUpdateReq;
+import com.palpal.dealightbe.domain.order.application.dto.response.OrderRes;
 import com.palpal.dealightbe.domain.order.application.dto.response.OrderStatusUpdateRes;
+import com.palpal.dealightbe.domain.order.application.dto.response.OrdersRes;
 import com.palpal.dealightbe.domain.order.domain.Order;
 import com.palpal.dealightbe.domain.order.domain.OrderRepository;
 import com.palpal.dealightbe.domain.order.domain.OrderStatus;
 import com.palpal.dealightbe.domain.store.domain.DayOff;
 import com.palpal.dealightbe.domain.store.domain.Store;
+import com.palpal.dealightbe.domain.store.domain.StoreRepository;
 import com.palpal.dealightbe.global.error.exception.BusinessException;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,6 +50,8 @@ class OrderServiceTest {
 	private OrderRepository orderRepository;
 	@Mock
 	private MemberRepository memberRepository;
+	@Mock
+	private StoreRepository storeRepository;
 
 	@InjectMocks
 	private OrderService orderService;
@@ -52,8 +64,8 @@ class OrderServiceTest {
 	public static final long MEMBER_ID = 1234L;
 	public static final long STORE_OWNER_ID = 1235L;
 
-	@BeforeAll
-	static void beforeAll() {
+	@BeforeEach
+	void beforeEach() {
 		member = Member.builder()
 			.providerId(MEMBER_ID)
 			.build();
@@ -335,13 +347,87 @@ class OrderServiceTest {
 		}
 	}
 
+	@Nested
+	@DisplayName("<업체의 주문 목록 조회>")
+	class findByStoreIdTest {
+		@Nested
+		@DisplayName("성공")
+		class Success {
+			@DisplayName("업체의 주문 목록을 조회할 수 있다")
+			@Test
+			void findByStoreId_success() {
+				// given
+				long storeId = 1L;
+
+				Order order1 = createOrder(LocalTime.of(18, 30), 30000);
+				Order order2 = createOrder(LocalTime.of(22, 0), 10000);
+
+				Slice<Order> ordersSlice = new SliceImpl<>(
+					Arrays.asList(order1, order2), PageRequest.of(0, 10), true
+				);
+
+				when(orderRepository.findAllByStoreId(anyLong(), any(), any()))
+					.thenReturn(ordersSlice);
+
+				when(storeRepository.findById(storeId))
+					.thenReturn(Optional.ofNullable(store));
+
+				// when
+				OrdersRes result = orderService.findAllByStoreId(storeId, storeOwner.getProviderId(), null,
+					PageRequest.of(0, 10));
+
+				// then
+				assertThat(result.orders(), hasSize(2));
+				assertThat(result.hasNext(), is(true));
+
+				Assertions.assertThat(result.orders().get(0))
+					.usingRecursiveComparison()
+					.isEqualTo(OrderRes.from(order1));
+				Assertions.assertThat(result.orders().get(1))
+					.usingRecursiveComparison()
+					.isEqualTo(OrderRes.from(order2));
+
+			}
+		}
+
+		@Nested
+		@DisplayName("실패")
+		class fail {
+			@DisplayName("업체의 주인이 아니면 주문 목록을 조회할 수 없다")
+			@Test
+			void findByStoreId_fail_not_owner() {
+				// given
+				long storeId = 1L;
+				long requesterId = member.getProviderId();
+
+				when(storeRepository.findById(storeId))
+					.thenReturn(Optional.ofNullable(store));
+
+				// when
+				// then
+				assertThrows(BusinessException.class, () ->
+					orderService.findAllByStoreId(storeId, requesterId, null, null));
+			}
+		}
+	}
+
+	private Order createOrder(LocalTime arrivalTime, int totalPrice) {
+		return Order.builder()
+			.demand("도착할 때까지 상품 냉장고에 보관 부탁드려요")
+			.arrivalTime(arrivalTime)
+			.store(store)
+			.member(member)
+			.totalPrice(totalPrice)
+			.build();
+	}
+
 	private Order createOrder() {
 		return Order.builder()
 			.demand("도착할 때까지 상품 냉장고에 보관 부탁드려요")
 			.arrivalTime(LocalTime.of(12, 30))
 			.store(store)
 			.member(member)
+			.totalPrice(10000)
 			.build();
 	}
-
 }
