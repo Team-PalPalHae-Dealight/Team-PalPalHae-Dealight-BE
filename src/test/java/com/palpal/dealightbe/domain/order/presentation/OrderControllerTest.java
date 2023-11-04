7cf4e0ad764bb59401e9da16b4d91802e694d972
SW-H <hsw08@naver.com>
2023-11-04 15:17:27 +0900

    docs : rest docs, 컨트롤러 테스트

--- src/test/java/com/palpal/dealightbe/domain/order/presentation/OrderControllerTest.java
+++ src/test/java/com/palpal/dealightbe/domain/order/presentation/OrderControllerTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
@@ -16,6 +18,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.requestF
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -29,13 +32,15 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -50,10 +55,16 @@ import com.palpal.dealightbe.domain.order.application.dto.response.OrderProductR
 import com.palpal.dealightbe.domain.order.application.dto.response.OrderProductsRes;
 import com.palpal.dealightbe.domain.order.application.dto.response.OrderRes;
 import com.palpal.dealightbe.domain.order.application.dto.response.OrderStatusUpdateRes;
+import com.palpal.dealightbe.domain.order.application.dto.response.OrdersRes;
 
 @AutoConfigureRestDocs
-@WebMvcTest(value = OrderController.class,
-	excludeAutoConfiguration = {SecurityAutoConfiguration.class, OAuth2ClientAutoConfiguration.class})
+@WebMvcTest(
+	value = OrderController.class,
+	excludeAutoConfiguration = {SecurityAutoConfiguration.class, OAuth2ClientAutoConfiguration.class}
+)
+// @AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(RestDocumentationExtension.class)
+
 public class OrderControllerTest {
 
 	@Autowired
@@ -92,10 +103,13 @@ public class OrderControllerTest {
 
 			// when
 			// then
-			mockMvc.perform(post(createApiPath + "/{memberProviderId}", 1).with(csrf())
-					.with(user("username").roles("MEMBER"))
-					.content(objectMapper.writeValueAsString(orderCreateReq))
-					.contentType(MediaType.APPLICATION_JSON))
+			mockMvc.perform(
+					post(createApiPath + "/{memberProviderId}", 1)
+						.with(csrf())
+						.with(user("username").roles("MEMBER"))
+						.content(objectMapper.writeValueAsString(orderCreateReq))
+						.contentType(APPLICATION_JSON)
+				)
 				.andDo(print())
 				.andExpect(status().isCreated())
 				.andExpect(jsonPath("$.orderId").value(1L))
@@ -115,7 +129,8 @@ public class OrderControllerTest {
 				.andExpect(jsonPath("$.status").value(RECEIVED.getText()))
 				.andDo(document("order/order-create-success", preprocessRequest(prettyPrint()),
 					preprocessResponse(prettyPrint()),
-					pathParameters(parameterWithName("memberProviderId").description("고객 카카오 토큰")), requestFields(
+					pathParameters(parameterWithName("memberProviderId").description("고객 카카오 토큰")),
+					requestFields(
 						fieldWithPath("orderProductsReq.orderProducts[]").type(JsonFieldType.ARRAY)
 							.description("주문한 상품 정보 목록"),
 						fieldWithPath("orderProductsReq.orderProducts[].itemId").type(JsonFieldType.NUMBER)
@@ -126,8 +141,10 @@ public class OrderControllerTest {
 						fieldWithPath("storeId").type(JsonFieldType.NUMBER).description("상품을 구매한 업체 아이디"),
 						fieldWithPath("demand").type(JsonFieldType.STRING).description("상품 구매시 작성한 고객의 요청 사항"),
 						fieldWithPath("arrivalTime").type(JsonFieldType.STRING).description("고객의 도착 예정 시간"),
-						fieldWithPath("totalPrice").type(JsonFieldType.NUMBER).description("주문 총 금액")),
-					responseFields(fieldWithPath("orderId").type(JsonFieldType.NUMBER).description("등록된 주문의 아이디"),
+						fieldWithPath("totalPrice").type(JsonFieldType.NUMBER).description("주문 총 금액")
+					),
+					responseFields(
+						fieldWithPath("orderId").type(JsonFieldType.NUMBER).description("등록된 주문의 아이디"),
 						fieldWithPath("storeId").type(JsonFieldType.NUMBER).description("주문이 이루어진 업체 아이디"),
 						fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("고객 아이디"),
 						fieldWithPath("storeName").type(JsonFieldType.STRING).description("업체 이름"),
@@ -168,7 +185,7 @@ public class OrderControllerTest {
 			mockMvc.perform(post(createApiPath + "/{memberProviderId}", 1).with(csrf())
 					.with(user("username").roles("MEMBER"))
 					.content(objectMapper.writeValueAsString(orderCreateReq))
-					.contentType(MediaType.APPLICATION_JSON))
+					.contentType(APPLICATION_JSON))
 				.andDo(print())
 				.andExpect(status().is4xxClientError())
 				.andExpect(result -> {
@@ -191,7 +208,7 @@ public class OrderControllerTest {
 			mockMvc.perform(post(createApiPath + "/{memberProviderId}", 1).with(csrf())
 					.with(user("username").roles("MEMBER"))
 					.content(objectMapper.writeValueAsString(orderCreateReq))
-					.contentType(MediaType.APPLICATION_JSON))
+					.contentType(APPLICATION_JSON))
 				.andDo(print())
 				.andExpect(status().is4xxClientError())
 				.andExpect(result -> {
@@ -207,14 +224,15 @@ public class OrderControllerTest {
 				new OrderProductsReq(List.of(new OrderProductReq(1L, 3))), 1L, "도착할 때까지 상품 냉장고에 보관 부탁드려요",
 				LocalTime.of(12, 30), 0);
 
-			given(orderService.create(any(OrderCreateReq.class), anyLong())).willReturn(orderRes);
+			given(orderService.create(any(OrderCreateReq.class), anyLong()))
+				.willReturn(orderRes);
 
 			// when
 			// then
 			mockMvc.perform(post(createApiPath + "/{memberProviderId}", 1).with(csrf())
 					.with(user("username").roles("MEMBER"))
 					.content(objectMapper.writeValueAsString(orderCreateReq))
-					.contentType(MediaType.APPLICATION_JSON))
+					.contentType(APPLICATION_JSON))
 				.andDo(print())
 				.andExpect(status().is4xxClientError())
 				.andExpect(result -> {
@@ -238,27 +256,39 @@ public class OrderControllerTest {
 			long orderId = 1L;
 			long memberProviderId = 1L;
 
-			given(orderService.updateStatus(anyLong(), any(OrderStatusUpdateReq.class), anyLong())).willReturn(
-				orderStatusUpdateRes);
+			given(orderService.updateStatus(anyLong(), any(OrderStatusUpdateReq.class), anyLong()))
+				.willReturn(orderStatusUpdateRes);
 
 			// when
 			// then
-			mockMvc.perform(patch(updateStatusApiPath, orderId, memberProviderId).with(csrf())
-					.with(user("username").roles("MEMBER"))
-					.content(objectMapper.writeValueAsString(orderStatusUpdateReq))
-					.contentType(MediaType.APPLICATION_JSON))
+			mockMvc.perform(
+					patch(updateStatusApiPath, orderId, memberProviderId)
+						.with(csrf())
+						.with(user("username").roles("MEMBER"))
+						.content(objectMapper.writeValueAsString(orderStatusUpdateReq))
+						.contentType(APPLICATION_JSON)
+				)
 				.andDo(print())
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.orderId").value(orderId))
 				.andExpect(jsonPath("$.status").value(orderStatusUpdateRes.status()))
-				.andDo(document("order/order-status-update-success", preprocessRequest(prettyPrint()),
-					preprocessResponse(prettyPrint()),
-					pathParameters(parameterWithName("orderId").description("상태 변경을 하고자 하는 주문의 아이디"),
-						parameterWithName("memberProviderId").description("고객 카카오 토큰")),
-					requestFields(fieldWithPath("status").type(JsonFieldType.STRING)
-						.description("변경 후의 주문 상태(CONFIRMED, COMPLETED, CANCELED)")),
-					responseFields(fieldWithPath("orderId").type(JsonFieldType.NUMBER).description("상태가 변경된 주문의 아이디"),
-						fieldWithPath("status").type(JsonFieldType.STRING).description("변경 완료된 후의 주문 상태"))));
+				.andDo(
+					document("order/order-status-update-success",
+						preprocessRequest(prettyPrint()),
+						preprocessResponse(prettyPrint()),
+						pathParameters(
+							parameterWithName("orderId").description("상태 변경을 하고자 하는 주문의 아이디"),
+							parameterWithName("memberProviderId").description("고객 카카오 토큰")
+						),
+						requestFields(
+							fieldWithPath("status").type(JsonFieldType.STRING)
+								.description("변경 후의 주문 상태(CONFIRMED, COMPLETED, CANCELED)")
+						),
+						responseFields(
+							fieldWithPath("orderId").type(JsonFieldType.NUMBER).description("상태가 변경된 주문의 아이디"),
+							fieldWithPath("status").type(JsonFieldType.STRING).description("변경 완료된 후의 주문 상태")
+						)
+					));
 		}
 
 		@Test
@@ -275,15 +305,113 @@ public class OrderControllerTest {
 
 			// when
 			// then
-			mockMvc.perform(patch(updateStatusApiPath, orderId, memberProviderId).with(csrf())
+			mockMvc.perform(patch(updateStatusApiPath, orderId, memberProviderId)
+					.with(csrf())
 					.with(user("username").roles("MEMBER"))
 					.content(objectMapper.writeValueAsString(invalidOrderStatusUpdateReq))
-					.contentType(MediaType.APPLICATION_JSON))
+					.contentType(APPLICATION_JSON))
 				.andDo(print())
 				.andExpect(status().is4xxClientError())
 				.andExpect(result -> {
 					assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException);
 				});
+		}
+	}
+
+	@Nested
+	@DisplayName("<주문 목록 조회>")
+	class findByStoreIdTest {
+		String findByStoreIdPath = "/api/orders/{memberProviderId}";
+
+		LocalDateTime createdAt = LocalDateTime.now();
+
+		OrderProductsRes productsRes = new OrderProductsRes(List.of(new OrderProductRes(1L, "달콤한 도넛", 5, 10000, 15000,
+			"https://team-08-bucket.s3.ap-northeast-2.amazonaws.com/donut")));
+
+		OrderProductRes productRes = productsRes.orderProducts().get(0);
+
+		OrderRes orderRes = new OrderRes(1L, 1L, 1L, "GS25", "도착할 때까지 상품 냉장고에 보관 부탁드려요", LocalTime.of(12, 30),
+			productsRes, 10000, createdAt, RECEIVED.getText());
+
+		OrdersRes ordersRes = new OrdersRes(List.of(orderRes), false);
+
+		@Test
+		@DisplayName("성공 - 업체의 주문 목록을 조회한다")
+		void findByStoreId_success() throws Exception {
+			// given
+			given(orderService.findAllByStoreId(anyLong(), anyLong(), any(), any(PageRequest.class)))
+				.willReturn(ordersRes);
+
+			// when
+			// then
+			mockMvc.perform(
+					get(findByStoreIdPath, 1)
+						.param("storeId", "1")
+						.with(user("username").roles("MEMBER"))
+				)
+				.andExpect(status().isOk())
+
+				.andExpect(jsonPath("$.orders[0].orderId").value(1L))
+				.andExpect(jsonPath("$.orders[0].storeId").value(1L))
+				.andExpect(jsonPath("$.orders[0].memberId").value(1L))
+				.andExpect(jsonPath("$.orders[0].storeName").value("GS25"))
+				.andExpect(jsonPath("$.orders[0].demand").value(orderRes.demand()))
+				.andExpect(jsonPath("$.orders[0].orderProductsRes.orderProducts[0].itemId").value(productRes.itemId()))
+				.andExpect(jsonPath("$.orders[0].orderProductsRes.orderProducts[0].name").value(productRes.name()))
+				.andExpect(jsonPath("$.orders[0].orderProductsRes.orderProducts[0].stock").value(productRes.stock()))
+				.andExpect(jsonPath("$.orders[0].orderProductsRes.orderProducts[0].discountPrice")
+					.value(productRes.discountPrice()))
+				.andExpect(jsonPath("$.orders[0].orderProductsRes.orderProducts[0].originalPrice")
+					.value(productRes.originalPrice()))
+				.andExpect(jsonPath("$.orders[0].orderProductsRes.orderProducts[0].image").value(productRes.image()))
+				.andExpect(jsonPath("$.orders[0].totalPrice").value(orderRes.totalPrice()))
+				.andExpect(jsonPath("$.orders[0].status").value(RECEIVED.getText()))
+				.andExpect(jsonPath("$.hasNext").value(false))
+				.andDo(
+					document("order/order-find-by-store-id-success",
+						preprocessRequest(prettyPrint()),
+						preprocessResponse(prettyPrint()),
+						pathParameters(
+							parameterWithName("memberProviderId").description("고객 카카오 토큰")
+						),
+						requestParameters(
+							parameterWithName("storeId").description("업체의 아이디"),
+							parameterWithName("status")
+								.description("주문 목록 중 보고자 하는 주문의 상태 - RECEIVED, CONFIRMED, COMPLETED, CANCELED")
+								.optional(),
+							parameterWithName("page").description("데이터 조회 시작 위치(==offset)").optional(),
+							parameterWithName("size").description("한 번에 조회할 데이터 개수(초기값:10)").optional()
+						),
+						responseFields(
+							fieldWithPath("orders").type(JsonFieldType.ARRAY).description("주문 목록"),
+							fieldWithPath("orders[].orderId").type(JsonFieldType.NUMBER).description("등록된 주문의 아이디"),
+							fieldWithPath("orders[].storeId").type(JsonFieldType.NUMBER).description("주문이 이루어진 업체 아이디"),
+							fieldWithPath("orders[].memberId").type(JsonFieldType.NUMBER).description("고객 아이디"),
+							fieldWithPath("orders[].storeName").type(JsonFieldType.STRING).description("업체 이름"),
+							fieldWithPath("orders[].demand").type(JsonFieldType.STRING).description("고객의 요구 사항"),
+							fieldWithPath("orders[].arrivalTime").type(JsonFieldType.STRING)
+								.description("고객의 도착 예정 시간"),
+							fieldWithPath("orders[].orderProductsRes.orderProducts[]").type(JsonFieldType.ARRAY)
+								.description("주문한 상품 목록"),
+							fieldWithPath("orders[].orderProductsRes.orderProducts[].itemId").type(JsonFieldType.NUMBER)
+								.description("상품 아이디"),
+							fieldWithPath("orders[].orderProductsRes.orderProducts[].name").type(JsonFieldType.STRING)
+								.description("상품명"),
+							fieldWithPath("orders[].orderProductsRes.orderProducts[].stock").type(JsonFieldType.NUMBER)
+								.description("상품 재고"),
+							fieldWithPath("orders[].orderProductsRes.orderProducts[].discountPrice")
+								.type(JsonFieldType.NUMBER)
+								.description("상품의 할인된 금액"),
+							fieldWithPath("orders[].orderProductsRes.orderProducts[].originalPrice")
+								.type(JsonFieldType.NUMBER)
+								.description("상품 원가"),
+							fieldWithPath("orders[].orderProductsRes.orderProducts[].image").type(JsonFieldType.STRING)
+								.description("상품 이미지"),
+							fieldWithPath("orders[].totalPrice").type(JsonFieldType.NUMBER).description("총 금액"),
+							fieldWithPath("orders[].createdAt").type(JsonFieldType.STRING).description("주문 완료 일자 및 시간"),
+							fieldWithPath("orders[].status").type(JsonFieldType.STRING).description("현재 주문 상태"),
+							fieldWithPath("hasNext").type(JsonFieldType.BOOLEAN).description("다음 데이터 존재 여부")))
+				);
 		}
 	}
 }
