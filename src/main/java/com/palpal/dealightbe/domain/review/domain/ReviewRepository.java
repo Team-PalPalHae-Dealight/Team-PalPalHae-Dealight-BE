46bf65546bc22b681c9b212bdc592153cd0ce143
SW-H <hsw08@naver.com>
2023-11-05 19:21:02 +0900

    feat : jpa repository

---
+++ src/main/java/com/palpal/dealightbe/domain/review/domain/ReviewRepository.java
@@ -0,0 +1,6 @@
+package com.palpal.dealightbe.domain.review.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
