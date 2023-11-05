e2008372606c2d5bf27a2f42a391e97989d0f6be
SW-H <hsw08@naver.com>
2023-11-05 23:59:53 +0900

    feat : 리뷰 통계 조회 쿼리문 작성

--- src/main/java/com/palpal/dealightbe/domain/review/domain/ReviewRepository.java
+++ src/main/java/com/palpal/dealightbe/domain/review/domain/ReviewRepository.java
@@ -1,6 +1,25 @@
 package com.palpal.dealightbe.domain.review.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.palpal.dealightbe.domain.review.application.dto.response.ReviewStatistics;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+	@Query(nativeQuery = true,
+		value = """
+				select
+			 	  	reviews.content as content,
+			 		count(reviews.id) as count
+			 	from
+			 		reviews left outer join orders on reviews.order_id = orders.id
+			 		left outer join members on orders.member_id = members.id
+			 			left outer join stores on orders.store_id = stores.id
+			 	where stores.id = :storeId
+			 	group by reviews.content
+			""")
+	List<ReviewStatistics> selectStatisticsByStoreId(Long storeId);
 }
