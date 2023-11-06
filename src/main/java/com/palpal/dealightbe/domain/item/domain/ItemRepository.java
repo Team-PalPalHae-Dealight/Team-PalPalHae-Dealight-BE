2eb9df4e976f198094330b042cb94c2449e82ebd
hyee0715 <hyee0715@naver.com>
2023-11-06 22:37:09 +0900

    refactor: ItemRepository 쿼리 nativeQuery로 변경

--- src/main/java/com/palpal/dealightbe/domain/item/domain/ItemRepository.java
+++ src/main/java/com/palpal/dealightbe/domain/item/domain/ItemRepository.java
@@ -10,39 +10,61 @@ public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositor
 
 	boolean existsByNameAndStoreId(String name, Long storeId);
 
-	@Query("SELECT i FROM Item i JOIN i.store s " +
-		"WHERE s.storeStatus = 'OPENED'" +
-		"AND (6371 * ACOS(COS(RADIANS(:yCoordinate)) " +
-		"    	* COS(RADIANS(s.address.yCoordinate)) " +
-		"		* COS(RADIANS(s.address.xCoordinate) - RADIANS(:xCoordinate)) " +
-		"       + SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(s.address.yCoordinate)))) < 3" +
-		"ORDER BY " +
-		"CASE " +
-		"  WHEN s.closeTime < CURRENT_TIME THEN " +
-		"    (24 * 60 * 60 + (HOUR(s.closeTime) * 3600 + MINUTE(s.closeTime) * 60 + SECOND(s.closeTime))) " +
-		"  ELSE " +
-		"    (HOUR(s.closeTime) * 3600 + MINUTE(s.closeTime) * 60 + SECOND(s.closeTime)) " +
-		"END ASC, i.updatedAt DESC")
+	@Query(value =
+		"""
+			SELECT * 
+			FROM items i 
+			LEFT JOIN stores s ON i.store_id = s.id 
+			LEFT JOIN addresses a ON s.address_id = a.id 
+			WHERE s.store_status = 'OPENED' 
+			AND (6371 * ACOS(COS(RADIANS(:yCoordinate)) 
+			    	* COS(RADIANS(a.y_coordinate)) 
+					* COS(RADIANS(a.x_coordinate) - RADIANS(:xCoordinate)) 
+			       + SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(a.y_coordinate)))) < 3 
+			ORDER BY 
+			    CASE 
+			        WHEN s.close_time < CURTIME() 
+			        	THEN (24 * 60 * 60 + (HOUR(s.close_time) * 3600 + MINUTE(s.close_time) * 60 + SECOND(s.close_time))) 
+			    	ELSE (HOUR(s.close_time) * 3600 + MINUTE(s.close_time) * 60 + SECOND(s.close_time)) 
+			    END ASC, i.updated_at DESC
+			""",
+		nativeQuery = true)
 	Page<Item> findAllByDeadline(@Param("xCoordinate") double xCoordinate, @Param("yCoordinate") double yCoordinate, Pageable pageable);
 
-	@Query("SELECT i FROM Item i JOIN i.store s " +
-		"WHERE s.storeStatus = 'OPENED'" +
-		"AND (6371 * ACOS(COS(RADIANS(:yCoordinate)) " +
-		"    	* COS(RADIANS(s.address.yCoordinate)) " +
-		"		* COS(RADIANS(s.address.xCoordinate) - RADIANS(:xCoordinate)) " +
-		"       + SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(s.address.yCoordinate)))) < 3" +
-		"ORDER BY (i.originalPrice - i.discountPrice) * 1.0 / i.originalPrice DESC, i.updatedAt DESC")
+	@Query(
+		value =
+			"""
+				SELECT * 
+				FROM items i 
+				LEFT JOIN stores s ON i.store_id = s.id 
+				LEFT JOIN addresses a ON s.address_id = a.id 
+				WHERE s.store_status = 'OPENED' 
+				AND (6371 * ACOS(COS(RADIANS(:yCoordinate)) 
+				    	* COS(RADIANS(a.y_coordinate)) 
+						* COS(RADIANS(a.x_coordinate) - RADIANS(:xCoordinate)) 
+				       + SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(a.y_coordinate)))) < 3 
+				ORDER BY (i.original_price - i.discount_price) * 1.0 / i.original_price DESC, i.updated_at DESC
+				""",
+		nativeQuery = true)
 	Page<Item> findAllByDiscountRate(@Param("xCoordinate") double xCoordinate, @Param("yCoordinate") double yCoordinate, Pageable pageable);
 
-	@Query("SELECT i FROM Item i JOIN i.store s " +
-		"WHERE s.storeStatus = 'OPENED'" +
-		"AND (6371 * ACOS(COS(RADIANS(:yCoordinate)) " +
-		"    	* COS(RADIANS(s.address.yCoordinate)) " +
-		"		* COS(RADIANS(s.address.xCoordinate) - RADIANS(:xCoordinate)) " +
-		"       + SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(s.address.yCoordinate)))) < 3" +
-		"ORDER BY (6371 * ACOS(COS(RADIANS(:yCoordinate)) " +
-		"    	* COS(RADIANS(s.address.yCoordinate)) " +
-		"		* COS(RADIANS(s.address.xCoordinate) - RADIANS(:xCoordinate)) " +
-		"       + SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(s.address.yCoordinate)))) ASC, i.updatedAt DESC")
+	@Query(
+		value =
+			"""
+				SELECT *
+				FROM items i 
+				LEFT JOIN stores s ON i.store_id = s.id 
+				LEFT JOIN addresses a ON s.address_id = a.id 
+				WHERE s.store_status = 'OPENED' 
+				AND (6371 * ACOS(COS(RADIANS(:yCoordinate)) 
+				    	* COS(RADIANS(a.y_coordinate)) 
+						* COS(RADIANS(a.x_coordinate) - RADIANS(:xCoordinate)) 
+				       + SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(a.y_coordinate)))) < 3 
+				ORDER BY (6371 * ACOS(COS(RADIANS(:yCoordinate)) 
+				    	* COS(RADIANS(a.y_coordinate)) 
+						* COS(RADIANS(a.x_coordinate) - RADIANS(:xCoordinate)) 
+				       + SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(a.y_coordinate)))) ASC, i.updated_at DESC
+				""",
+		nativeQuery = true)
 	Page<Item> findAllByDistance(@Param("xCoordinate") double xCoordinate, @Param("yCoordinate") double yCoordinate, Pageable pageable);
 }
