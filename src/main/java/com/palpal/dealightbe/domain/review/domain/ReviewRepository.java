3f1984ef1b2e65d8c5a50819cfd6f57c27ee775d
SW-H <hsw08@naver.com>
2023-11-06 18:34:48 +0900

    refactor

--- src/main/java/com/palpal/dealightbe/domain/review/domain/ReviewRepository.java
+++ src/main/java/com/palpal/dealightbe/domain/review/domain/ReviewRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.palpal.dealightbe.domain.review.application.dto.response.ReviewStatistics;
 
@@ -18,10 +19,10 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 			 		reviews left outer join orders on reviews.order_id = orders.id
 			 		left outer join members on orders.member_id = members.id
 			 			left outer join stores on orders.store_id = stores.id
-			 	where stores.id = :storeId
+			 	where stores.id = :store_id
 			 	group by reviews.content
 			""")
-	List<ReviewStatistics> selectStatisticsByStoreId(Long storeId);
+	List<ReviewStatistics> selectStatisticsByStoreId(@Param("store_id") Long storeId);
 
 	List<Review> findAllByOrderId(Long id);
 }
