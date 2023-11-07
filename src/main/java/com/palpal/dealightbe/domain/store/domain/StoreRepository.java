f5b0c0b25a64ee3ba1102d07efac77d6ffda9e23
Hongseob Lee <sjun2918@naver.com>
2023-11-07 17:37:55 +0900

    feat: 고객의 업체 등록 유무 - 서비스 코드 작성

--- src/main/java/com/palpal/dealightbe/domain/store/domain/StoreRepository.java
+++ src/main/java/com/palpal/dealightbe/domain/store/domain/StoreRepository.java
@@ -3,8 +3,13 @@ package com.palpal.dealightbe.domain.store.domain;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
 	Optional<Store> findByMemberId(Long memberId);
+
+	@Query("SELECT s FROM Store s JOIN FETCH s.member m WHERE m.providerId = :providerId")
+	Optional<Store> findByMemberProviderId(@Param("providerId") Long providerId);
 }
