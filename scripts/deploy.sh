bff99569ece45c2804cc0436f942316c1a7d4698
ysng_ysng <yeasung67@gmail.com>
2023-11-09 01:47:04 +0900

    ci: deploy.sh 스크립트에서 명령어 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,20 +9,20 @@ EXIST_BLUE=$(docker ps | grep ${DOCKER_APP_NAME}-blue)
 # green이 실행중이면 blue up
 if [ -z "$EXIST_BLUE" ]; then
   echo "Green 컨테이너가 실행 중이므로 Blue 컨테이너를 실행(UP)합니다..."
-  docker compose -f docker-compose.blue.yml up -d
+  sudo docker compose -f docker-compose.blue.yml up -d
 
   sleep 30
 
-  docker compose -f docker-compose.green.yml down
-  docker image prune -af # 사용하지 않는 이미지 삭제
+  sudo docker compose -f docker-compose.green.yml down
+  sudo docker image prune -af # 사용하지 않는 이미지 삭제
 
 # blue가 실행중이면 green up
 else
   echo "Blue 컨테이너가 실행 중이므로 Green 컨테이너를 실행(UP)합니다..."
-  docker compose -f docker-compose.green.yml up -d
+  sudo docker compose -f docker-compose.green.yml up -d
 
   sleep 30
 
-  docker compose -f docker-compose.blue.yml down
-  docker image prune -af
+  sudo docker compose -f docker-compose.blue.yml down
+  sudo docker image prune -af
 fi
