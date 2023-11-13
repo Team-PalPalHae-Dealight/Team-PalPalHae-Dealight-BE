15f317012aae6735ff34db7fa0b5b49d013578e0
Hongseob Lee <sjun2918@naver.com>
2023-11-13 18:54:24 +0900

    feat: 업체 검색 쿼리문 수정

--- src/main/java/com/palpal/dealightbe/domain/store/domain/StoreRepository.java
+++ src/main/java/com/palpal/dealightbe/domain/store/domain/StoreRepository.java
@@ -14,61 +14,64 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 	@Query("SELECT s FROM Store s JOIN FETCH s.member m WHERE m.providerId = :providerId")
 	Optional<Store> findByMemberProviderId(@Param("providerId") Long providerId);
 
-	@Query(value = """
-		SELECT s
-		FROM Store s 
-		JOIN FETCH s.items i 
-		WHERE 
-		   (6371 * acos(cos(radians(:xCoordinate)) * cos(radians(s.x_coordinate)) * cos(radians(s.y_coordinate) - radians(:yCoordinate)) + 
-		   sin(radians(:xCoordinate)) * sin(radians(s.x_coordinate)))) <= 3 
-		   AND (s.storeStatus = 'OPEN') 
-		   AND (s.name LIKE %:keyword% OR i.name LIKE %:keyword%) 
-		GROUP BY s.id 
-		ORDER BY distance ASC
-		""", nativeQuery = true)
+
+	@Query(value =
+		"""
+				SELECT s.*
+				FROM stores s
+					JOIN addresses a ON s.address_id = a.id
+					LEFT JOIN items i ON s.id = i.store_id
+					WHERE s.store_status = 'OPENED'
+					AND (6371 * ACOS(COS(RADIANS(:yCoordinate))
+						    	* COS(RADIANS(a.y_coordinate))
+								* COS(RADIANS(a.x_coordinate) - RADIANS(:xCoordinate))
+						       + SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(a.y_coordinate)))) <= 3
+					AND (s.name LIKE %:keyword% OR i.name LIKE %:keyword%)
+				ORDER BY (6371 * ACOS(COS(RADIANS(:yCoordinate))
+						    	* COS(RADIANS(a.y_coordinate))
+								* COS(RADIANS(a.x_coordinate) - RADIANS(:xCoordinate))
+						       + SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(a.y_coordinate)))) ASC, i.updated_at DESC
+			""", nativeQuery = true)
 	Slice<Store> findByDistanceWithin3Km(
 		@Param("xCoordinate") double xCoordinate,
 		@Param("yCoordinate") double yCoordinate,
 		@Param("keyword") String keyword
 	);
 
-	@Query(value = """
-		SELECT s
-		FROM Store s 
-		JOIN FETCH s.items i 
-		WHERE 
-		   (6371 * acos(cos(radians(:xCoordinate)) * cos(radians(s.x_coordinate)) * cos(radians(s.y_coordinate) - radians(:yCoordinate)) + 
-		   sin(radians(:xCoordinate)) * sin(radians(s.x_coordinate)))) <= 3 
-		   AND (s.storeStatus = 'OPEN') 
-		   AND (s.name LIKE %:keyword% OR i.name LIKE %:keyword%) 
-		GROUP BY s.id 
-		ORDER BY 
-		   CASE 
-		       WHEN s.closeTime < CURRENT_TIME() THEN (24 * 60 * 60 + (HOUR(s.closeTime) * 3600 + MINUTE(s.closeTime) * 60 + SECOND(s.closeTime))) 
-		       ELSE (HOUR(s.closeTime) * 3600 + MINUTE(s.closeTime) * 60 + SECOND(s.closeTime)) 
-		   END ASC, 
-		   i.updatedAt DESC
-		""", nativeQuery = true)
+	@Query(value =
+		"""
+					SELECT s.*
+					FROM stores s
+					JOIN addresses a ON s.address_id = a.id
+					LEFT JOIN items i ON s.id = i.store_id
+					WHERE s.store_status = 'OPENED'
+					AND (6371 * ACOS(COS(RADIANS(:yCoordinate))
+								* COS(RADIANS(a.y_coordinate))
+								* COS(RADIANS(a.x_coordinate) - RADIANS(:xCoordinate))
+								+ SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(a.y_coordinate)))) <= 3
+					AND (s.name LIKE %:keyword% OR i.name LIKE %:keyword%)	
+					ORDER BY ABS(EXTRACT(EPOCH FROM s.close_time - CURRENT_TIME)) ASC
+			""", nativeQuery = true)
 	Slice<Store> findByDeadLine(
 		@Param("xCoordinate") double xCoordinate,
 		@Param("yCoordinate") double yCoordinate,
 		@Param("keyword") String keyword
 	);
 
-	@Query(value = """
-		SELECT s
-		FROM Store s 
-		JOIN FETCH s.items i 
-		WHERE 
-		   (6371 * acos(cos(radians(:xCoordinate)) * cos(radians(s.x_coordinate)) * cos(radians(s.y_coordinate) - radians(:yCoordinate)) + 
-		   sin(radians(:xCoordinate)) * sin(radians(s.x_coordinate)))) <= 3 
-		   AND (s.storeStatus = 'OPEN') 
-		   AND (s.name LIKE %:keyword% OR i.name LIKE %:keyword%) 
-		GROUP BY s.id 
-		ORDER BY 
-		   (i.originalPrice - i.discountPrice) * 1.0 / i.originalPrice DESC, 
-		   i.updatedAt DESC
-		""", nativeQuery = true)
+	@Query(value =
+		"""
+					SELECT s.*
+					FROM stores s
+					JOIN addresses a ON s.address_id = a.id
+					LEFT JOIN items i ON s.id = i.store_id
+					WHERE s.store_status = 'OPENED'
+					AND (6371 * ACOS(COS(RADIANS(:yCoordinate))
+								* COS(RADIANS(a.y_coordinate))
+								* COS(RADIANS(a.x_coordinate) - RADIANS(:xCoordinate))
+								+ SIN(RADIANS(:yCoordinate)) * SIN(RADIANS(a.y_coordinate)))) <= 3
+					AND (s.name LIKE %:keyword% OR i.name LIKE %:keyword%)	
+					ORDER BY (i.original_price - i.discount_price) * 1.0 / i.original_price DESC, i.updated_at DESC
+			""", nativeQuery = true)
 	Slice<Store> findByDiscountRate(
 		@Param("xCoordinate") double xCoordinate,
 		@Param("yCoordinate") double yCoordinate,
