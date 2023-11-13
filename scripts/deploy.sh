6645d47f09a018b0f03e51978c08c7c31b965de7
ysng_ysng <yeasung67@gmail.com>
2023-11-14 08:36:46 +0900

    ci: REST Docs 최신화 및 실시간 배포를 위한 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+cd /home/ubuntu/app
 
+# REST DOCS 문서 파일 이동
+source_dir="/home/ubuntu/app/docs"
+target_dir="/home/ubuntu/docs"
+
+find "$source_dir" -name "*.html" -exec mv {} "$target_dir" \;
+
+echo "모든 .html 파일을 $target_dir로 옮겼습니다."
+
+# Blue-Green 배포 실행
 # ubuntu 프로세스 매니저 버그 해결을 위해 추가
 sudo aa-remove-unknown
-
-cd /home/ubuntu/app
 DOCKER_APP_NAME=dealight
 
 # 실행중인 blue가 있는지
