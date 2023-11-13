007bc6da99a1679539a593738263c5bc75770027
hyee0715 <hyee0715@naver.com>
2023-11-13 18:01:03 +0900

    feat: 본인이 등록한 업체의 상품은 장바구니에 담을 수 없도록 설정

--- src/main/java/com/palpal/dealightbe/domain/cart/application/CartService.java
+++ src/main/java/com/palpal/dealightbe/domain/cart/application/CartService.java
@@ -15,10 +15,13 @@ import com.palpal.dealightbe.domain.cart.domain.Cart;
 import com.palpal.dealightbe.domain.cart.domain.CartRepository;
 import com.palpal.dealightbe.domain.item.domain.Item;
 import com.palpal.dealightbe.domain.item.domain.ItemRepository;
+import com.palpal.dealightbe.domain.member.domain.Member;
+import com.palpal.dealightbe.domain.store.domain.Store;
 import com.palpal.dealightbe.global.error.exception.BusinessException;
 import com.palpal.dealightbe.global.error.exception.EntityNotFoundException;
 
 import static com.palpal.dealightbe.global.error.ErrorCode.ANOTHER_STORE_ITEM_ALREADY_EXISTS_IN_THE_CART;
+import static com.palpal.dealightbe.global.error.ErrorCode.INVALID_ATTEMPT_TO_ADD_OWN_STORE_ITEM_TO_CART;
 import static com.palpal.dealightbe.global.error.ErrorCode.NOT_FOUND_ITEM;
 
 @Transactional
@@ -33,6 +36,8 @@ public class CartService {
 	public CartRes checkAndAddItem(Long providerId, Long itemId) {
 		Item item = getItem(itemId);
 
+		validateOwnStoreItem(providerId, item);
+
 		List<Cart> carts = cartRepository.findAllByMemberProviderId(providerId);
 
 		validateAnotherStoreItemExistence(carts, item.getStore().getId());
@@ -43,11 +48,23 @@ public class CartService {
 	public CartRes clearAndAddItem(Long providerId, Long itemId) {
 		Item item = getItem(itemId);
 
+		validateOwnStoreItem(providerId, item);
+
 		List<Cart> carts = cartRepository.findAllByMemberProviderId(providerId);
 
 		clearAnotherStoreItem(carts, item.getStore().getId());
 
 		return addItem(providerId, itemId);
+	}
+
+	private void validateOwnStoreItem(Long providerId, Item item) {
+		Store store = item.getStore();
+		Member member = store.getMember();
+
+		if (Objects.equals(member.getProviderId(), providerId)) {
+			log.warn("POST:CREATE:INVALID_ATTEMPT_TO_ADD_OWN_STORE_ITEM_TO_CART : providerId = {}, itemId = {}", providerId, item.getId());
+			throw new BusinessException(INVALID_ATTEMPT_TO_ADD_OWN_STORE_ITEM_TO_CART);
+		}
 	}
 
 	private void clearAnotherStoreItem(List<Cart> carts, Long attemptedStoreId) {
