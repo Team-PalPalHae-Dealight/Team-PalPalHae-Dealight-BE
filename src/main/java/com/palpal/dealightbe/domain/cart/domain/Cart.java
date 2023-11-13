2ac4590ca4faee5f9469b819ddb495561f8ab3d9
hyee0715 <hyee0715@naver.com>
2023-11-13 17:26:22 +0900

    feat: Cart 클래스 구현

---
+++ src/main/java/com/palpal/dealightbe/domain/cart/domain/Cart.java
@@ -0,0 +1,91 @@
+package com.palpal.dealightbe.domain.cart.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import com.palpal.dealightbe.global.error.exception.BusinessException;
+
+import static com.palpal.dealightbe.global.error.ErrorCode.*;
+
+@Slf4j
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash("carts")
+public class Cart {
+
+	public static int INITIAL_QUANTITY = 1;
+
+	@Id
+	private Long id;
+
+	@Indexed
+	private Long itemId;
+
+	private Long storeId;
+
+	@Indexed
+	private Long memberProviderId;
+
+	private String itemName;
+
+	private int stock;
+
+	private int discountPrice;
+
+	private String itemImage;
+
+	private int quantity;
+
+	private String storeName;
+
+	private LocalTime storeCloseTime;
+
+	@TimeToLive
+	private Long expiration;
+
+	@Builder
+	public Cart(Long itemId, Long storeId, Long memberProviderId, String itemName, int stock, int discountPrice, String itemImage, String storeName, LocalTime storeCloseTime) {
+		this.itemId = itemId;
+		this.storeId = storeId;
+		this.memberProviderId = memberProviderId;
+		this.itemName = itemName;
+		this.stock = stock;
+		this.discountPrice = discountPrice;
+		this.itemImage = itemImage;
+		this.quantity = INITIAL_QUANTITY;
+		this.storeName = storeName;
+		this.storeCloseTime = storeCloseTime;
+		this.expiration = calculateExpiration();
+	}
+
+	public void updateExpiration() {
+		this.expiration = calculateExpiration();
+	}
+
+	private Long calculateExpiration() {
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		LocalDateTime closeDateTime = getCloseDateTime(currentDateTime);
+
+		return currentDateTime.until(closeDateTime, ChronoUnit.SECONDS);
+	}
+
+	private LocalDateTime getCloseDateTime(LocalDateTime currentDateTime) {
+		if (this.storeCloseTime.isBefore(currentDateTime.toLocalTime())) {
+			return currentDateTime.toLocalDate().atTime(this.storeCloseTime).plusDays(1);
+		}
+
+		return currentDateTime.toLocalDate().atTime(this.storeCloseTime);
+	}
+}
