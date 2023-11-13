8b19d4a6879f35253dc5022144a73972f4bfed0a
kimihiqq <kimihiqq@gmail.com>
2023-11-13 17:39:03 +0900

    feat: Notification 도메인 생성

---
+++ src/main/java/com/palpal/dealightbe/domain/notification/domain/Notification.java
@@ -0,0 +1,71 @@
+package com.palpal.dealightbe.domain.notification.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.palpal.dealightbe.domain.member.domain.Member;
+import com.palpal.dealightbe.domain.order.domain.Order;
+import com.palpal.dealightbe.domain.store.domain.Store;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "notifications")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id")
+	private Store store;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	@Enumerated(EnumType.STRING)
+	private NotificationType type;
+
+	private String message;
+
+	private boolean isRead = false;
+
+	@Builder
+	public Notification(Member member, Store store, Order order, NotificationType type, String message) {
+		this.member = member;
+		this.store = store;
+		this.order = order;
+		this.type = type;
+		this.message = message;
+	}
+
+	public void markAsRead() {
+		this.isRead = true;
+	}
+
+	public enum NotificationType {
+		ORDER_RECEIVED,
+		ORDER_CONFIRMED,
+		ORDER_COMPLETED,
+		ORDER_CANCELED
+	}
+}
