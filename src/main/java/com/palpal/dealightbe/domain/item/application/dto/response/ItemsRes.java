1005baa10520a58e72a8bb271fe30fa944655f30
hyee0715 <hyee0715@naver.com>
2023-11-14 18:15:25 +0900

    hotfix: 상품 목록 페이징 Slice 처리

--- src/main/java/com/palpal/dealightbe/domain/item/application/dto/response/ItemsRes.java
+++ src/main/java/com/palpal/dealightbe/domain/item/application/dto/response/ItemsRes.java
@@ -3,17 +3,21 @@ package com.palpal.dealightbe.domain.item.application.dto.response;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
 
 import com.palpal.dealightbe.domain.item.domain.Item;
 
 public record ItemsRes(
-	List<ItemRes> items
+	List<ItemRes> items,
+	boolean hasNext
 ) {
 
-	public static ItemsRes from(Page<Item> items) {
+	public static ItemsRes from(Slice<Item> items) {
 
-		return new ItemsRes(items.stream()
+		List<ItemRes> itemResList = items.stream()
 			.map(ItemRes::from)
-			.toList());
+			.toList();
+
+		return new ItemsRes(itemResList, items.hasNext());
 	}
 }
