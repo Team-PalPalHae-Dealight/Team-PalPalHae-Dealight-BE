8b6a8bffa8c09b6ea7243bd5e1908576c9f3d98b
Hongseob Lee <sjun2918@naver.com>
2023-11-15 12:38:59 +0900

    test: 업체 검색 컨트롤러 테스트 및 RestDocs 작성

--- src/test/java/com/palpal/dealightbe/domain/store/presentation/StoreControllerTest.java
+++ src/test/java/com/palpal/dealightbe/domain/store/presentation/StoreControllerTest.java
@@ -1,9 +1,11 @@
 package com.palpal.dealightbe.domain.store.presentation;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
@@ -20,12 +22,14 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.subsecti
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
@@ -38,6 +42,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
@@ -55,7 +61,9 @@ import com.palpal.dealightbe.domain.store.application.dto.request.StoreUpdateReq
 import com.palpal.dealightbe.domain.store.application.dto.response.StoreByMemberRes;
 import com.palpal.dealightbe.domain.store.application.dto.response.StoreCreateRes;
 import com.palpal.dealightbe.domain.store.application.dto.response.StoreInfoRes;
+import com.palpal.dealightbe.domain.store.application.dto.response.StoreInfoSliceRes;
 import com.palpal.dealightbe.domain.store.application.dto.response.StoreStatusRes;
+import com.palpal.dealightbe.domain.store.application.dto.response.StoresInfoSliceRes;
 import com.palpal.dealightbe.domain.store.domain.DayOff;
 import com.palpal.dealightbe.domain.store.domain.StoreStatus;
 import com.palpal.dealightbe.global.error.ErrorCode;
@@ -548,6 +556,263 @@ class StoreControllerTest {
 					fieldWithPath("code").type(STRING).description("오류 코드"),
 					fieldWithPath("errors").type(ARRAY).description("오류 목록"),
 					fieldWithPath("message").type(STRING).description("오류 메시지")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("업체 검색 - 기본 정렬(정렬조건 추가X -> 거리순 정렬)")
+	void SearchByDefault() throws Exception {
+
+		//given
+		double xCoordinate = 127.0279372;
+		double yCoordinate = 37.4980136;
+		String keyword = "떡볶이";
+		int size = 5;
+		int page = 0;
+		Pageable pageable = PageRequest.of(page, size);
+
+		StoreInfoSliceRes store1 = new StoreInfoSliceRes(1L, "천하장사", LocalTime.of(19, 00), "image");
+		StoreInfoSliceRes store2 = new StoreInfoSliceRes(10L, "떡볶이 파는집", LocalTime.of(21, 30), "image");
+		StoresInfoSliceRes storesInfoSliceRes = new StoresInfoSliceRes(
+			List.of(store1, store2), false);
+
+		when(storeService.search(anyDouble(), anyDouble(), any(), any(), any()))
+			.thenReturn(storesInfoSliceRes);
+
+		//when -> then
+		mockMvc.perform(RestDocumentationRequestBuilders.get("/api/stores/search")
+				.contentType(APPLICATION_JSON)
+				.param("x-coordinate", String.valueOf(xCoordinate))
+				.param("y-coordinate", String.valueOf(yCoordinate))
+				.param("keyword", keyword)
+				.param("size", String.valueOf(size))
+				.param("page", String.valueOf(page)))
+			.andExpect(status().isOk())
+			.andDo(print())
+			.andDo(document("store/search-by-default-option",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestParameters(
+					List.of(parameterWithName("x-coordinate").description("경도"),
+						parameterWithName("y-coordinate").description("위도"),
+						parameterWithName("keyword").description("검색어"),
+						parameterWithName("size").description("한 페이지 당 상품 목록 개수"),
+						parameterWithName("page").description("페이지 번호")
+					)),
+				responseFields(
+					fieldWithPath("storeInfoSliceRes[0].storeId").description("조회된 업체 ID"),
+					fieldWithPath("storeInfoSliceRes[0].name").description("조회된 업체 이름"),
+					fieldWithPath("storeInfoSliceRes[0].closeTime").description("조회된 업체 마감 시간"),
+					fieldWithPath("storeInfoSliceRes[0].image").description("조회된 업체 이미지"),
+					fieldWithPath("hasNext").description("추가 결과 여부")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("업체 검색 - 거리순")
+	void SearchByDefaultOption() throws Exception {
+
+		//given
+		double xCoordinate = 127.0279372;
+		double yCoordinate = 37.4980136;
+		String sortBy = "distance";
+		String keyword = "떡볶이";
+		int size = 5;
+		int page = 0;
+		Pageable pageable = PageRequest.of(page, size);
+
+		StoreInfoSliceRes store1 = new StoreInfoSliceRes(1L, "천하장사", LocalTime.of(19, 00), "image");
+		StoreInfoSliceRes store2 = new StoreInfoSliceRes(10L, "떡볶이 파는집", LocalTime.of(21, 30), "image");
+		StoresInfoSliceRes storesInfoSliceRes = new StoresInfoSliceRes(
+			List.of(store1, store2), false);
+
+		when(storeService.search(anyDouble(), anyDouble(), eq(keyword), eq(sortBy), eq(pageable)))
+			.thenReturn(storesInfoSliceRes);
+
+		//when -> then
+		mockMvc.perform(RestDocumentationRequestBuilders.get("/api/stores/search")
+				.contentType(APPLICATION_JSON)
+				.param("x-coordinate", String.valueOf(xCoordinate))
+				.param("y-coordinate", String.valueOf(yCoordinate))
+				.param("keyword", keyword)
+				.param("sort-by", sortBy)
+				.param("size", String.valueOf(size))
+				.param("page", String.valueOf(page)))
+			.andExpect(status().isOk())
+			.andDo(print())
+			.andDo(document("store/search-by-distance",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestParameters(
+					List.of(parameterWithName("x-coordinate").description("경도"),
+						parameterWithName("y-coordinate").description("위도"),
+						parameterWithName("keyword").description("검색어"),
+						parameterWithName("sort-by").description("정렬 기준(기본 및 거리순) : distance"),
+						parameterWithName("size").description("한 페이지 당 상품 목록 개수"),
+						parameterWithName("page").description("페이지 번호")
+					)),
+				responseFields(
+					fieldWithPath("storeInfoSliceRes[0].storeId").description("조회된 업체 ID"),
+					fieldWithPath("storeInfoSliceRes[0].name").description("조회된 업체 이름"),
+					fieldWithPath("storeInfoSliceRes[0].closeTime").description("조회된 업체 마감 시간"),
+					fieldWithPath("storeInfoSliceRes[0].image").description("조회된 업체 이미지"),
+					fieldWithPath("hasNext").description("추가 결과 여부")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("업체 검색 - 마감 임박순")
+	void SearchByDeadline() throws Exception {
+
+		//given
+		double xCoordinate = 127.0279372;
+		double yCoordinate = 37.4980136;
+		String sortBy = "deadline";
+		String keyword = "떡볶이";
+		int size = 5;
+		int page = 0;
+		Pageable pageable = PageRequest.of(page, size);
+
+		StoreInfoSliceRes store1 = new StoreInfoSliceRes(1L, "천하장사", LocalTime.of(23, 00), "image");
+		StoreInfoSliceRes store2 = new StoreInfoSliceRes(10L, "떡볶이 파는집", LocalTime.of(19, 30), "image2");
+		StoresInfoSliceRes storesInfoSliceRes = new StoresInfoSliceRes(
+			List.of(store2, store1), false);
+
+		when(storeService.search(anyDouble(), anyDouble(), any(), any(), any()))
+			.thenReturn(storesInfoSliceRes);
+
+		//when -> then
+		mockMvc.perform(RestDocumentationRequestBuilders.get("/api/stores/search")
+				.contentType(APPLICATION_JSON)
+				.param("x-coordinate", String.valueOf(xCoordinate))
+				.param("y-coordinate", String.valueOf(yCoordinate))
+				.param("keyword", keyword)
+				.param("sort-by", sortBy)
+				.param("size", String.valueOf(size))
+				.param("page", String.valueOf(page)))
+			.andExpect(status().isOk())
+			.andDo(print())
+			.andDo(document("store/search-by-deadline",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestParameters(
+					List.of(parameterWithName("x-coordinate").description("경도"),
+						parameterWithName("y-coordinate").description("위도"),
+						parameterWithName("keyword").description("검색어"),
+						parameterWithName("sort-by").description("정렬 기준(마감 임박순) : deadline"),
+						parameterWithName("size").description("한 페이지 당 상품 목록 개수"),
+						parameterWithName("page").description("페이지 번호")
+					)),
+				responseFields(
+					fieldWithPath("storeInfoSliceRes[0].storeId").description("조회된 업체 ID"),
+					fieldWithPath("storeInfoSliceRes[0].name").description("조회된 업체 이름"),
+					fieldWithPath("storeInfoSliceRes[0].closeTime").description("조회된 업체 마감 시간"),
+					fieldWithPath("storeInfoSliceRes[0].image").description("조회된 업체 이미지"),
+					fieldWithPath("hasNext").description("추가 결과 여부")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("업체 검색 - 할인률 순")
+	void SearchByDiscountRate() throws Exception {
+
+		//given
+		double xCoordinate = 127.0279372;
+		double yCoordinate = 37.4980136;
+		String sortBy = "discount-rate";
+		String keyword = "떡볶이";
+		int size = 5;
+		int page = 0;
+		Pageable pageable = PageRequest.of(page, size);
+
+		StoreInfoSliceRes store1 = new StoreInfoSliceRes(1L, "천하장사", LocalTime.of(23, 00), "image");
+		StoreInfoSliceRes store2 = new StoreInfoSliceRes(10L, "떡볶이 파는집", LocalTime.of(19, 30), "image2");
+		StoresInfoSliceRes storesInfoSliceRes = new StoresInfoSliceRes(
+			List.of(store2, store1), false);
+
+		when(storeService.search(anyDouble(), anyDouble(), any(), any(), any()))
+			.thenReturn(storesInfoSliceRes);
+
+		//when -> then
+		mockMvc.perform(RestDocumentationRequestBuilders.get("/api/stores/search")
+				.contentType(APPLICATION_JSON)
+				.param("x-coordinate", String.valueOf(xCoordinate))
+				.param("y-coordinate", String.valueOf(yCoordinate))
+				.param("keyword", keyword)
+				.param("sort-by", sortBy)
+				.param("size", String.valueOf(size))
+				.param("page", String.valueOf(page)))
+			.andExpect(status().isOk())
+			.andDo(print())
+			.andDo(document("store/search-by-discount-rate",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestParameters(
+					List.of(parameterWithName("x-coordinate").description("경도"),
+						parameterWithName("y-coordinate").description("위도"),
+						parameterWithName("keyword").description("검색어"),
+						parameterWithName("sort-by").description("정렬 기준(할인률순) : discount-rate"),
+						parameterWithName("size").description("한 페이지 당 상품 목록 개수"),
+						parameterWithName("page").description("페이지 번호")
+					)),
+				responseFields(
+					fieldWithPath("storeInfoSliceRes[0].storeId").description("조회된 업체 ID"),
+					fieldWithPath("storeInfoSliceRes[0].name").description("조회된 업체 이름"),
+					fieldWithPath("storeInfoSliceRes[0].closeTime").description("조회된 업체 마감 시간"),
+					fieldWithPath("storeInfoSliceRes[0].image").description("조회된 업체 이미지"),
+					fieldWithPath("hasNext").description("추가 결과 여부")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("업체 검색 - 만족하는 조건이 없을시 빈 배열")
+	void SearchByNoCondition() throws Exception {
+
+		//given
+		double xCoordinate = 127.0279372;
+		double yCoordinate = 37.4980136;
+		String sortBy = "discount-rate";
+		String keyword = "감자";
+		int size = 5;
+		int page = 0;
+		Pageable pageable = PageRequest.of(page, size);
+
+		StoresInfoSliceRes storesInfoSliceRes = new StoresInfoSliceRes(List.of(), false);
+
+		when(storeService.search(anyDouble(), anyDouble(), any(), any(), any()))
+			.thenReturn(storesInfoSliceRes);
+
+		//when -> then
+		mockMvc.perform(RestDocumentationRequestBuilders.get("/api/stores/search")
+				.contentType(APPLICATION_JSON)
+				.param("x-coordinate", String.valueOf(xCoordinate))
+				.param("y-coordinate", String.valueOf(yCoordinate))
+				.param("keyword", keyword)
+				.param("sort-by", sortBy)
+				.param("size", String.valueOf(size))
+				.param("page", String.valueOf(page)))
+			.andExpect(status().isOk())
+			.andDo(print())
+			.andDo(document("store/search-by-no-condition",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestParameters(
+					List.of(parameterWithName("x-coordinate").description("경도"),
+						parameterWithName("y-coordinate").description("위도"),
+						parameterWithName("keyword").description("검색어"),
+						parameterWithName("sort-by").description("정렬 기준(할인률순) : discount-rate"),
+						parameterWithName("size").description("한 페이지 당 상품 목록 개수"),
+						parameterWithName("page").description("페이지 번호")
+					)),
+				responseFields(
+					fieldWithPath("storeInfoSliceRes").description("조회된 업체 정보 목록"),
+					fieldWithPath("storeInfoSliceRes[]").description("업체 정보 목록 (빈 배열 가능)"),
+					fieldWithPath("hasNext").description("추가 결과 여부")
 				)
 			));
 	}
