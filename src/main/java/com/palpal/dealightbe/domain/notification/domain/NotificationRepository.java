f74a637053998b38fd7a708b7f85f8335847c28a
kimihiqq <kimihiqq@gmail.com>
2023-11-15 16:56:40 +0900

    refactor: Notification 리파지터리 수정
    - @Repository 제거

--- src/main/java/com/palpal/dealightbe/domain/notification/domain/NotificationRepository.java
+++ src/main/java/com/palpal/dealightbe/domain/notification/domain/NotificationRepository.java
@@ -1,9 +1,7 @@
 package com.palpal.dealightbe.domain.notification.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
 }
