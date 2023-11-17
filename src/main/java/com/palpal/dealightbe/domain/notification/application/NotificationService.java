d144c9d36c451317a60d763311f25f8748458a36
kimihiqq <kimihiqq@gmail.com>
2023-11-17 16:34:54 +0900

    feat: Notification 서비스 내 notificationRes를 send하도록 구현

--- src/main/java/com/palpal/dealightbe/domain/notification/application/NotificationService.java
+++ src/main/java/com/palpal/dealightbe/domain/notification/application/NotificationService.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.palpal.dealightbe.domain.member.domain.Member;
 import com.palpal.dealightbe.domain.member.domain.RoleType;
+import com.palpal.dealightbe.domain.notification.application.dto.response.NotificationRes;
 import com.palpal.dealightbe.domain.notification.domain.EmitterRepository;
 import com.palpal.dealightbe.domain.notification.domain.Notification;
 import com.palpal.dealightbe.domain.notification.domain.NotificationRepository;
@@ -20,7 +21,9 @@ import com.palpal.dealightbe.domain.store.domain.Store;
 import com.palpal.dealightbe.global.error.exception.BusinessException;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
@@ -38,10 +41,12 @@ public class NotificationService {
 		String emitterId = getEmitterId(id, userType);
 
 		SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
-		emitterRepository.save(emitterId, emitter);
 
 		emitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
 		emitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
+
+		emitterRepository.save(emitterId, emitter);
+
 		sendEventToEmitter(emitter, emitterId, "EventStream Created. [" + userType + "Id=" + id + "]");
 
 		if (!lastEventId.isEmpty()) {
@@ -52,19 +57,38 @@ public class NotificationService {
 	}
 
 	private void resendMissedEvents(Long id, String userType, String lastEventId, SseEmitter emitter) {
-		Map<String, String> events = emitterRepository.findAllEventCacheStartWithId(userType + "_" + id);
+		Map<String, Object> events = emitterRepository.findAllEventCacheStartWithId(userType + "_" + id);
 		events.entrySet().stream()
 			.filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
 			.forEach(entry -> sendEventToEmitter(emitter, entry.getKey(), entry.getValue()));
 	}
 
-	private void sendEventToEmitter(SseEmitter emitter, String emitterId, String data) {
+	private void sendEventToEmitter(SseEmitter emitter, String emitterId, Object data) {
+
 		try {
-			emitter.send(SseEmitter.event().id(emitterId).name("orderNotification").data(data));
+			if (data instanceof NotificationRes) {
+				// JSON 형태의 객체인 경우
+				emitter.send(SseEmitter.event().id(emitterId).name("orderNotification").data(data));
+			} else {
+				// 문자열인 경우
+				String jsonData = "{\"message\":\"" + data.toString() + "\"}";
+				emitter.send(SseEmitter.event().id(emitterId).name("orderNotification").data(jsonData));
+			}
 		} catch (IOException exception) {
 			emitterRepository.deleteById(emitterId);
 			throw new BusinessException(SSE_STREAM_ERROR);
 		}
+	}
+
+	private void sendNotification(Long id, Notification notification, String userType) {
+		// userType과 id를 기반으로 emitter를 찾고 이벤트 전송
+		String emitterKeyPrefix = userType + "_" + id;
+
+		Map<String, SseEmitter> emitters = emitterRepository.findAllStartWithById(emitterKeyPrefix);
+		emitters.forEach((key, emitter) -> {
+			emitterRepository.saveEventCache(key, notification);
+			sendEventToEmitter(emitter, key, NotificationRes.from(notification));
+		});
 	}
 
 	@Transactional
@@ -72,48 +96,37 @@ public class NotificationService {
 		// Notification 메시지 생성
 		String message = Notification.createMessage(orderStatus, order);
 
+		// Notification 저장
+		Notification notification = createNotification(member, store, order, message);
+		notificationRepository.save(notification);
+
 		// 알림 대상에 따라 SseEmitter에 이벤트 전송
 		switch (orderStatus) {
-			case RECEIVED:
+			case RECEIVED ->
 				// member가 주문을 했으므로, store에게 알림을 보냄
-				sendNotification(store.getId(), message, "STORE");
-				break;
-			case CONFIRMED:
+				sendNotification(store.getId(), notification, "store");
+			case CONFIRMED ->
 				// store가 주문을 확인했으므로, member에게 알림을 보냄
-				sendNotification(member.getId(), message, "MEMBER");
-				break;
-			case COMPLETED:
+				sendNotification(member.getId(), notification, "member");
+			case COMPLETED -> {
 				// 주문이 완료되었으므로, member와 store 모두에게 알림을 보냄
-				sendNotification(member.getId(), message, "MEMBER");
-				sendNotification(store.getId(), message, "STORE");
-				break;
-			case CANCELED:
-				// 주문이 취소되었으므로, store에게 알림을 보냄
-				sendNotification(store.getId(), message, "STORE");
-				break;
+				sendNotification(member.getId(), notification, "member");
+				sendNotification(store.getId(), notification, "store");
+			}
+			case CANCELED -> {
+				// 주문이 취소되었으므로, member와 store 모두에게 알림을 보냄
+				sendNotification(store.getId(), notification, "store");
+				sendNotification(member.getId(), notification, "member");
+			}
 		}
-
-		// Notification 저장
-		Notification notification = createNotification(member, store, order, orderStatus);
-		notificationRepository.save(notification);
 	}
 
-	private void sendNotification(Long id, String message, String userType) {
-		// userType과 id를 기반으로 emitter를 찾고 이벤트 전송
-		String emitterKeyPrefix = userType + "_" + id;
-		Map<String, SseEmitter> emitters = emitterRepository.findAllStartWithById(emitterKeyPrefix);
-		emitters.forEach((key, emitter) -> {
-			emitterRepository.saveEventCache(key, message);
-			sendEventToEmitter(emitter, key, message);
-		});
-	}
-
-	private Notification createNotification(Member member, Store store, Order order, OrderStatus type) {
+	private Notification createNotification(Member member, Store store, Order order, String content) {
 		return Notification.builder()
 			.member(member)
 			.store(store)
 			.order(order)
-			.type(type)
+			.content(content)
 			.build();
 	}
 }
