9a5fc0069f3f83ec7a741a8664f8212604ca208e
kimihiqq <kimihiqq@gmail.com>
2023-11-23 17:59:35 +0900

    feat: SchedulerConfig 추가

---
+++ src/main/java/com/palpal/dealightbe/config/SchedulerConfig.java
@@ -0,0 +1,20 @@
+package com.palpal.dealightbe.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+	private final int POOL_SIZE = 10;
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+		final ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+		threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+		threadPoolTaskScheduler.initialize();
+
+		scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+	}
+}
