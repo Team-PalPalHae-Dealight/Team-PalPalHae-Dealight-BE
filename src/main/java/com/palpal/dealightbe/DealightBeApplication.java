225ccca075b244f21eebe434aa3c5fc7a6589589
kimihiqq <kimihiqq@gmail.com>
2023-11-24 16:07:08 +0900

    refactor: @EnableScheduling 어노테이션 이동
    - 기존의 DealightBeApplication에서 SchedulerConfig로 이동

--- src/main/java/com/palpal/dealightbe/DealightBeApplication.java
+++ src/main/java/com/palpal/dealightbe/DealightBeApplication.java
@@ -2,10 +2,8 @@ package com.palpal.dealightbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class DealightBeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/palpal/dealightbe/config/SchedulerConfig.java
+++ src/main/java/com/palpal/dealightbe/config/SchedulerConfig.java
@@ -1,10 +1,12 @@
 package com.palpal.dealightbe.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+@EnableScheduling
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
 	private static final int POOL_SIZE = 10;
