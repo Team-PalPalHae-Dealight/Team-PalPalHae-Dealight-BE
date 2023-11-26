a612f946ee9af28fc669b291841ec16f72e0b28a
hyee0715 <hyee0715@naver.com>
2023-11-27 00:39:43 +0900

    refactor: ItemRepository 페치 조인 적용

--- src/main/java/com/palpal/dealightbe/domain/item/domain/ItemRepository.java
+++ src/main/java/com/palpal/dealightbe/domain/item/domain/ItemRepository.java
@@ -3,6 +3,8 @@ package com.palpal.dealightbe.domain.item.domain;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
 
@@ -11,4 +13,7 @@ public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositor
 	Optional<Item> findByNameAndStoreId(String name, Long storeId);
 
 	void deleteAllByStoreId(Long storeId);
+
+	@Query("SELECT i FROM Item i JOIN FETCH i.store s JOIN FETCH s.address WHERE i.id = :id")
+	Optional<Item> findById(@Param("id") Long id);
 }
