7a43c6e6ad3c1badbc4418e15e30749a0095511c
SW-H <hsw08@naver.com>
2023-11-28 14:36:51 +0900

    feat : 리뷰 response 값 변경

--- src/main/java/com/palpal/dealightbe/domain/review/application/dto/response/StoreReviewsRes.java
+++ src/main/java/com/palpal/dealightbe/domain/review/application/dto/response/StoreReviewsRes.java
@@ -2,6 +2,8 @@ package com.palpal.dealightbe.domain.review.application.dto.response;
 
 import java.util.List;
 
+import com.palpal.dealightbe.domain.review.domain.ReviewContent;
+
 public record StoreReviewsRes(
 	Long storeId,
 	List<StoreReviewRes> reviews
@@ -9,7 +11,9 @@ public record StoreReviewsRes(
 	public static StoreReviewsRes of(long storeId, List<ReviewStatistics> reviews) {
 		return new StoreReviewsRes(storeId,
 			reviews.stream()
-				.map(r -> new StoreReviewRes(r.getContent(), r.getCount()))
-				.toList());
+				.map(r -> new StoreReviewRes(
+					ReviewContent.valueOf(r.getContent()).getMessage(),
+					r.getCount())
+				).toList());
 	}
 }
