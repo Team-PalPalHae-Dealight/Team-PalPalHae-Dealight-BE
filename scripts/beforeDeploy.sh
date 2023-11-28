3f51e1165f1efa542b1a05b97f715ebf12539a96
Dev-Yesung <yeasung67@gmail.com>
2023-11-28 16:04:04 +0900

    beforeDeploy.sh에서 구버전 Apparmor 관련 설정 삭제

--- scripts/beforeDeploy.sh
+++ scripts/beforeDeploy.sh
@@ -8,12 +8,6 @@ target_dir="/home/ubuntu/docs"
 sudo find "$source_dir" -name "*.html" -exec mv {} "$target_dir" \;
 echo "모든 .html 파일을 $target_dir로 옮겼습니다."
 
-# ubuntu 프로세스 매니저 버그 해결을 위해 추가
-sudo aa-remove-unknown
-
-# 폴더에 권한 추가
-sudo -R 777 /home/ubuntu/app
-
 # docker-compose.infra를 실행하여 꺼진 컨테이너가 있으면 다시 실행
 echo "docker-compose.infra를 실행합니다..."
 sudo docker compose -f docker-compose.infra.yml up -d
