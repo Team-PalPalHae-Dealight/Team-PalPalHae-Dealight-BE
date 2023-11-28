88586eb492c9b9612985a6d3e503182994813b8b
Hongseob Lee <sjun2918@naver.com>
2023-11-28 22:42:12 +0900

    feat: ES에 RDB 데이터의 넣어주기 위한 UpdatedStore 엔티티 및 레파지토리 구성

---
+++ src/main/java/com/palpal/dealightbe/domain/store/domain/UpdatedStoreRepository.java
@@ -0,0 +1,6 @@
+package com.palpal.dealightbe.domain.store.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UpdatedStoreRepository extends JpaRepository<UpdatedStore, Long> {
+}
