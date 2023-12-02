9b2f374e796eb26d4adbc712c525c1f22a450777
SW-H <hsw08@naver.com>
2023-12-03 05:36:10 +0900

    feat : 일정 시간 주기로 상품/주문 삭제 - spring schedule

--- src/main/java/com/palpal/dealightbe/domain/notification/application/NotificationService.java
+++ src/main/java/com/palpal/dealightbe/domain/notification/application/NotificationService.java
@@ -9,7 +9,6 @@ import static com.palpal.dealightbe.global.error.ErrorCode.SSE_STREAM_ERROR;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Map;
 
@@ -21,7 +20,6 @@ import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -54,7 +52,6 @@ import lombok.extern.slf4j.Slf4j;
 public class NotificationService {
 	private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
 
-	private static final String DEFAULT_SCHEDULING_TIME = "0 0 2 ? * TUE";  //매주 화요일 새벽 2시에 진행
 	private final NotificationRepository notificationRepository;
 	private final MemberRepository memberRepository;
 	private final StoreRepository storeRepository;
@@ -142,14 +139,6 @@ public class NotificationService {
 		});
 	}
 
-	@Transactional
-	@Scheduled(cron = DEFAULT_SCHEDULING_TIME)
-	public void cleanupReadNotifications() {
-		notificationRepository.deleteReadNotifications();
-
-		log.info("RUN:CLEANUP_READ_NOTIFICATIONS:TIME : {}", LocalDateTime.now());
-		log.info("RUN:CLEANUP_READ_NOTIFICATIONS:THREAD : {}", Thread.currentThread().getName());
-	}
 
 	private void resendMissedEvents(Long id, String userType, String emitterId, String lastEventId,
 		SseEmitter emitter) {
