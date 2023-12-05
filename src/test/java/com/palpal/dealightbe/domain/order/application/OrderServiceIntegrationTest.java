635a16bd3c59635849d970d44078b43e929141ed
kimihiqq <kimihiqq@gmail.com>
2023-12-06 00:19:43 +0900

    refactor: OderServiceIntegrationTest 애너테이션 수정

--- src/test/java/com/palpal/dealightbe/domain/order/application/OrderServiceIntegrationTest.java
+++ src/test/java/com/palpal/dealightbe/domain/order/application/OrderServiceIntegrationTest.java
@@ -14,10 +14,8 @@ import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.annotation.Import;
 
 import com.palpal.dealightbe.common.IntegrationTest;
-import com.palpal.dealightbe.config.RedisConfig;
 import com.palpal.dealightbe.domain.address.domain.Address;
 import com.palpal.dealightbe.domain.item.domain.Item;
 import com.palpal.dealightbe.domain.member.domain.Member;
@@ -32,7 +30,6 @@ import com.palpal.dealightbe.domain.store.domain.DayOff;
 import com.palpal.dealightbe.domain.store.domain.Store;
 import com.palpal.dealightbe.global.error.exception.BusinessException;
 
-@Import(RedisConfig.class)
 public class OrderServiceIntegrationTest extends IntegrationTest {
 
 	@Nested
