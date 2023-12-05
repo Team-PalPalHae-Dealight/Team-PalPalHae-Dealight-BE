428d130b4c70bac6c72eb1142213b8bf8ba7701f
kimihiqq <kimihiqq@gmail.com>
2023-12-05 13:14:28 +0900

    fix: 누락된 알림 로직 복구

--- src/main/java/com/palpal/dealightbe/domain/order/application/OrderService.java
+++ src/main/java/com/palpal/dealightbe/domain/order/application/OrderService.java
@@ -20,6 +20,7 @@ import com.palpal.dealightbe.domain.item.domain.Item;
 import com.palpal.dealightbe.domain.item.domain.ItemRepository;
 import com.palpal.dealightbe.domain.member.domain.Member;
 import com.palpal.dealightbe.domain.member.domain.MemberRepository;
+import com.palpal.dealightbe.domain.notification.application.NotificationService;
 import com.palpal.dealightbe.domain.order.application.dto.request.OrderCreateReq;
 import com.palpal.dealightbe.domain.order.application.dto.request.OrderProductReq;
 import com.palpal.dealightbe.domain.order.application.dto.request.OrderStatusUpdateReq;
@@ -30,6 +31,7 @@ import com.palpal.dealightbe.domain.order.domain.Order;
 import com.palpal.dealightbe.domain.order.domain.OrderItem;
 import com.palpal.dealightbe.domain.order.domain.OrderItemRepository;
 import com.palpal.dealightbe.domain.order.domain.OrderRepository;
+import com.palpal.dealightbe.domain.order.domain.OrderStatus;
 import com.palpal.dealightbe.domain.store.domain.Store;
 import com.palpal.dealightbe.domain.store.domain.StoreRepository;
 import com.palpal.dealightbe.global.error.exception.BusinessException;
@@ -49,6 +51,7 @@ public class OrderService {
 	private final StoreRepository storeRepository;
 	private final ItemRepository itemRepository;
 	private final OrderItemRepository orderItemRepository;
+	private final NotificationService notificationService;
 
 	public OrderRes create(OrderCreateReq orderCreateReq, Long memberProviderId) {
 		long storeId = orderCreateReq.storeId();
@@ -62,15 +65,19 @@ public class OrderService {
 		order.addOrderItems(orderItems);
 		orderRepository.save(order);
 
+		notificationService.send(member, store, order, OrderStatus.RECEIVED);
+
 		return OrderRes.from(order);
 	}
 
 	public OrderStatusUpdateRes updateStatus(Long orderId, OrderStatusUpdateReq request, Long memberProviderId) {
 		Member member = getMember(memberProviderId);
 		Order order = getOrder(orderId);
+		Store store = getStore(order.getStore().getId());
 
 		String changedStatus = request.status();
 		order.changeStatus(member, changedStatus);
+		notificationService.send(member, store, order, OrderStatus.valueOf(changedStatus));
 
 		return OrderStatusUpdateRes.from(order);
 	}
